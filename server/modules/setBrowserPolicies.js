5aa8708d81be66135fc6c90eac8d1efe39f70892
Joe Burdick <josephdburdick@gmail.com>
2015-10-28 00:24:11 -0400

    Refactor browser policies.

--- server/modules/setBrowserPolicies.js
+++ server/modules/setBrowserPolicies.js
@@ -1,27 +1,8 @@
 let setBrowserPolicies = () => {
-  // BrowserPolicy.content.allowOriginForAll('*.googleapis.com');
-  // BrowserPolicy.content.allowOriginForAll('*.gstatic.com');
-  // BrowserPolicy.content.allowEval('https://ajax.googleapis.com');
-  // //Content-Security-Policy: script-src 'self' https://apis.google.com
-  // //Content-Security-Policy: script-src 'self' https://apis.google.com
-  // /*
-  // "script-src 'self' 'unsafe-inline'".
-  // */
-  //
-  // // BrowserPolicy.framing.disallow();
-  // // BrowserPolicy.content.disallowInlineScripts();
-  // // BrowserPolicy.content.disallowEval();
-  // BrowserPolicy.content.allowInlineStyles();
-  // BrowserPolicy.content.allowFontDataUrl();
 
   var trusted = [
     '*.googleapis.com',
-    'mts0.googleapis.com',
-    'csi.gstatic.com',
-    'maps.googleapis.com',
-    'googleapis.com',
-    'gstatic.com',
-    'ajax.googleapis.com',
+    '*.gstatic.com',
     'google-analytics.com'
   ];
 
