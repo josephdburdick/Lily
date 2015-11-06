abd622a57fd7200115d6a649bc18f5e72f984f57
Joe Burdick <josephdburdick@gmail.com>
2015-11-06 03:50:03 -0500

    Make separate collection for users' settings. Closes #1.

--- server/publications/user-settings.js
+++ server/publications/user-settings.js
@@ -1,7 +1,12 @@
-Meteor.publish('userSettings', function (maxRows) {
-  debugger;
-  if (this.userId) {
-    return Meteor.users.find({ _id: this.userId }, { fields: {'settings':1}});
+Meteor.publish( 'userSettings', function () {
+  if ( this.userId ) {
+    return Settings.find( {
+      userId: this.userId
+    }, {
+      fields: {
+        'settings': 1
+      }
+    } );
   }
   this.ready();
-});
+} );
