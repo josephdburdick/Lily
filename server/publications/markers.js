f5adac9e78081c91a57d53396eb2f26c28d67557
Joe <josephdburdick@gmail.com>
2015-12-07 01:54:57 -0500

    Refactoring of marker publications.

--- server/publications/markers.js
+++ server/publications/markers.js
@@ -39,6 +39,45 @@ Meteor.publish('allPublicMarkers', function () {
   this.ready();
 });
 
+
+Meteor.publish('nearestMarkersByPoint', function (coords) {
+  if (this.userId) {
+    check(coords, Modules.both.validateCoordinates);
+    let ownerIds;
+    let markerCursor = Markers.find({
+      'coordinates': {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [coords.lng, coords.lat],
+            $maxDistance: Math.round(3218.688 / 2) //Math.round(params.distanceLimit),
+          },
+          spherical: true
+        }
+      }
+    }, {
+      limit: 40
+    });
+    ownerIds = markerCursor.map(function (marker) {
+      return marker.ownerId;
+    });
+
+    return [
+      markerCursor,
+      Venues.find({
+        _id: {
+          $in: ownerIds
+        }
+      }),
+      Networks.find({
+        ownerId: {
+          $in: ownerIds
+        }
+      })
+    ];
+  }
+});
+
 // Meteor.publish('nearestMarkersByMapBounds', function (box) {
 //   if (this.userId) {
 //     console.log(box);
@@ -85,41 +124,3 @@ Meteor.publish('allPublicMarkers', function () {
 //     ];
 //   }
 // });
-
-Meteor.publish('nearestMarkersByPoint', function (coords) {
-  if (this.userId) {
-    check(coords, Modules.both.validateCoordinates);
-    let ownerIds;
-    let markerCursor = Markers.find({
-      'coordinates': {
-        $near: {
-          $geometry: {
-            type: "Point",
-            coordinates: [coords.lng, coords.lat],
-            $maxDistance: Math.round(3218.688 / 2) //Math.round(params.distanceLimit),
-          },
-          spherical: true
-        }
-      }
-    }, {
-      limit: 40
-    });
-    ownerIds = markerCursor.map(function (marker) {
-      return marker.ownerId;
-    });
-
-    return [
-      markerCursor,
-      Venues.find({
-        _id: {
-          $in: ownerIds
-        }
-      }),
-      Networks.find({
-        ownerId: {
-          $in: ownerIds
-        }
-      })
-    ];
-  }
-});
