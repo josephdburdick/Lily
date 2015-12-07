1825dadb10ec4370369ed5e7fe81a11b4e408e1d
Joe <josephdburdick@gmail.com>
2015-12-07 01:25:57 -0500

    Refactor network insert check.

--- both/methods/insert/networks.js
+++ both/methods/insert/networks.js
@@ -1,12 +1,14 @@
 Meteor.methods({
   insertNetwork(obj) {
-    check(obj, Object);
-    check(obj.ownerId, String);
-    check(obj.name, String);
-    check(obj.password, Match.any);
-    check(obj.hasPassword, Boolean);
-    check(obj.isPublic, Boolean);
-    check(obj.verified, Boolean);
+    check(obj, {
+      ownerId: String,
+      name: String,
+      password: Match.any,
+      hasPassword: Boolean,
+      isPublic: Boolean,
+      verified: Boolean
+    });
+
     try {
       var documentId = Networks.insert(obj);
       return documentId;
